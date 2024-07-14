57a6c48c85bf4702845b2e1c601a993d11064bc4
Gabriel2340 <gabrielepure1104@gmail.com>
2024-07-14 14:25:42 +0300

    memoria facuta pe octeti

--- calc/calc.srcs/sources_1/new/IM.v
+++ calc/calc.srcs/sources_1/new/IM.v
@@ -5,19 +5,18 @@ module IM(
     adr,
     instr
     );
-    parameter N = 32;
-    parameter M = 32;
     
-    input [M-1:0] adr;
-    output reg [N-1:0] instr;
+    input [31:0] adr;
+    output reg [31:0] instr;
     
-    reg [N-1:0] mem [99:0];
+    reg [7:0] mem [399:0];
     
     initial begin
         $readmemh("instr.mem", mem);
     end
     
     always @(*) begin
-        instr = mem[adr];
+        instr = {mem[adr + 3], mem[adr + 2], mem[adr + 1], mem[adr]};
     end
+    
 endmodule
