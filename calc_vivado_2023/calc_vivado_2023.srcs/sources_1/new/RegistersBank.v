1ce1586860de1326650e20a45fff42c13d161aa2
Gabriel Epure <gabrielepure1104@gmail.com>
2024-07-15 12:56:24 +0300

    registrii initializati cu 0 din RB

--- calc_vivado_2023/calc_vivado_2023.srcs/sources_1/new/RegistersBank.v
+++ calc_vivado_2023/calc_vivado_2023.srcs/sources_1/new/RegistersBank.v
@@ -10,6 +10,8 @@ module RegistersBank(
     RD1,
     RD2
     );
+    
+    integer i;
     input [4:0] RA1;
     input [4:0] RA2;
     input [4:0] WA;
@@ -19,6 +21,12 @@ module RegistersBank(
     output reg [31:0] RD1;
     output reg [31:0] RD2;
     reg [31:0] registers [31:0];
+    
+    initial begin
+        for (i = 0; i <= 31; i = i + 1) begin
+            registers[i] = 0;
+        end
+    end
     
     always @(posedge clk) begin
         RD1 = registers[RA1];
