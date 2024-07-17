6bad02b65a311d63219f5f2db1cd97d4037b21b9
Gabriel Epure <gabrielepure1104@gmail.com>
2024-07-17 13:04:16 +0300

    adaugare assign in loc de alwyas pentru negedge la RegistersBank

--- calc/calc.srcs/sources_1/new/RegistersBank.v
+++ calc/calc.srcs/sources_1/new/RegistersBank.v
@@ -18,19 +18,19 @@ module RegistersBank(
     input [31:0] WD;
     input REGWRITE;
     input clk;
-    output reg [31:0] RD1;
-    output reg [31:0] RD2;
+    output wire [31:0] RD1;
+    output wire [31:0] RD2;
     reg [31:0] registers [31:0];
+    
+    assign RD1 = registers[RA1];
+    assign RD2 = registers[RA2];
     
     initial begin
         for (i = 0; i <= 31; i = i + 1) begin
             registers[i] = 0;
         end
     end
-    always @(negedge clk) begin
-        RD1 <= registers[RA1];
-        RD2 <= registers[RA2];
-    end
+   
     always @(posedge clk) begin
         if (REGWRITE) begin // poate trebuie schimbat pe negedge
             registers[WA] <= WD;
