b207d47745969fb5b51fb058f4d0715130e47e5c
Gabriel2340 <gabrielepure1104@gmail.com>
2024-07-21 19:58:11 +0300

    implementare la mai multe operatii de stocare a memoriei in DM

--- calc/calc.srcs/sources_1/new/DM.v
+++ calc/calc.srcs/sources_1/new/DM.v
@@ -2,35 +2,33 @@
 
 module DM(
     adr,
+    RD,
     WD,
     clk,
-    MEMWRITE,
-    RD
+    MEMWRITE
     );
-    integer i;
-    parameter MAX_MEM = 32'hffff;
     input [31:0] adr;
     input [31:0] WD;
     input clk;
-    input MEMWRITE;
+    input [1:0] MEMWRITE;
     output wire [31:0] RD;
-    reg [7:0] mem [MAX_MEM-1:0];
+    reg [7:0] mem [65535:0];
     
     assign RD = {mem[adr], mem[adr + 1], mem[adr + 2], mem[adr + 3]};
     
-    initial begin
-        for (i = 0; i < MAX_MEM; i = i + 1) begin
-            mem[i] = 0;
-        end
-    end
-    
-    
     always @(posedge clk) begin
-        if (MEMWRITE) begin
+        if (MEMWRITE == 3) begin // SW
             mem[adr] <= WD[31:24];
             mem[adr + 1] <= WD[23:16];
             mem[adr + 2] <= WD[15:8];
             mem[adr + 3] <= WD[7:0];
+        end
+        if (MEMWRITE == 2) begin // SH
+            mem[adr] <= WD[15:8];
+            mem[adr + 1] <= WD[7:0];
+        end
+        if (MEMWRITE == 1) begin // SB
+            mem[adr] <= WD[7:0];
         end
     end
 endmodule
